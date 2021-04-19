44fbde8ed71895b869f0483a2cd5eb8ff4ccf19a
Ray Koziel <ray.koziel@gmail.com>
2021-04-19 10:53:26 -0400

    Initial push to Github

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+//const Joi = require('joi');
+const genres = require('./routes/genres');
+const customers = require('./routes/customers');
+const app = express();
+
+mongoose.connect('mongodb://localhost/vidly')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect...', err));
+
+app.use(express.json());
+app.use('/api/genres', genres);
+app.use('/api/customers', customers);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
---
+++ routes/genres.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const { Genre, validate } = require('../models/genres'); 
+
+router.get('/', async (req, res) => {
+  const genres = await Genre.find().sort('name');
+  res.send(genres);
+});
+router.get('/:id', async (req, res) => {
+  const genre = await Genre.findById(req.params.id);
+  if (!genre) return res.status(404).send(`Genre with id ${req.params.id} not found`);
+  res.send(genre);
+});
+router.post('/', async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  let genre = new Genre({
+    name: req.body.name
+  });
+  genre = await genre.save();
+  return res.send(genre);  
+});
+router.put('/:id', async (req, res) => {
+  // const genre = genres.find(g => g.id === parseInt(req.params.id));
+  // if (!genre) return res.status(404).send(`Genre with id ${req.params.id} not found`);
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  const genre = await Genre.findByIdAndUpdate(req.params.id, {
+    $set: {
+      name: req.body.name,
+    }
+  }, { new: true});
+  if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+  res.send(genre);
+});
+router.delete('/:id', async (req, res) => {
+  // const genre = genres.find(g => g.id === parseInt(req.params.id));
+  const genre = await Genre.findByIdAndRemove(req.params.id);
+  if (!genre) return res.status(404).send(`Genre with id ${req.params.id} not found`);
+  res.send(genre);
+});
+
+module.exports = router;
